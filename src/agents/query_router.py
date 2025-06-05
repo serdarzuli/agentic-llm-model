71ac2d29379e8164f9172549a607eb24f42a18d6
ZuliSerdar <serdar.zuli@gmail.com>
2025-06-05 17:25:12 +0200

    Add query router module to route user queries to the appropriate model

---
+++ src/agents/query_router.py
@@ -0,0 +1,29 @@
+from retrievers.hybrid_retriever import get_relecant_documents
+from models.model_selector import call_llm
+from typing import List
+
+def route_query(query: str, prompt_template: str = None) -> str:
+    """
+    Verilen sorguyu uygun bir modele yönlendirir ve sonuçları döndürür.
+    
+    query: Sorgu metni
+    """
+    # İlgili belgeleri al
+    documents = get_relecant_documents(query)
+
+    context = "\n\n".join([doc.page_content for doc in documents])
+
+    prompt = f"""
+    You are an expert assistant. Based on the following documents, answer the user's question.
+
+    === USER QUESTION ===
+    {query}
+
+    === CONTEXT DOCUMENTS ===
+    {context}
+
+    Answer:"""
+
+    answer = call_llm(prompt)
+
+    return answer
